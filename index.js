02ffcb0a8f0b49baed010d5ce9811b9bcba46149
barath-7 <79738124+barath-7@users.noreply.github.com>
2022-10-30 12:05:54 +0530

    final solution for fcc filemetadata project

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var cors = require('cors');
 require('dotenv').config()
 
 var app = express();
+const multer = require('multer');
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +12,17 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const upload = multer({ dest: './data/uploads/' , limits:{fileSize:1000000}}) //1MB file size
+app.post('/api/fileanalyse',upload.any(),(req,res)=>{
+  
+  let { originalname = '', mimetype = '', size = 0 } = req.files[0] || {};
+  return res.json({
+    "name":originalname,
+    "type":mimetype,
+    "size":size
 
+  })
+})
 
 
 const port = process.env.PORT || 3000;
